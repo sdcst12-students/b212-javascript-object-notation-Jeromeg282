ecb8542f374f95bcaa959a24c8f52f4226ab75f4
Jeromeg282 <113056411+Jeromeg282@users.noreply.github.com>
2023-11-05 22:42:47 -0800

    upd

--- task01.py
+++ task01.py
@@ -7,3 +7,17 @@
 
 import json
 
+
+def findmax(input_list):
+    return max(input_list)
+
+
+file_names = ['task01a.txt', 'task01b.txt', 'task01c.txt']
+for file_name in file_names:
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+
+        largest=findmax(data)
+        taskname=file_name.split('.')[0]
+        print(f"Largest number in {taskname} is : {largest}")
+        
--- task02.py
+++ task02.py
@@ -7,7 +7,19 @@ import json
 # You can google the difference between POST and GET requests
 
 req = requests.get('http://sdcaf.hungrybeagle.com/menu.php')
-data = req.text
+data = req.json()
+
+
+if 'menu' in data:
+    weekly_menu=data['menu']
+    for day_menu in weekly_menu:
+        print(f"Menu for {day_menu['dayname']} ({day_menu['date']}):")
+        print(f"Soup: {day_menu['soup']}")
+        print(f"Short Order: {day_menu['shortorder']}")
+        print(f"Entree: {day_menu['entree']}")
+        print(f"Notes: {day_menu['notes']}\n")
+
+
 
 # Use the json encoded data that is retrieved from this website and print out the weekly menu
 # You will need to decipher the json decoded data to determine what information the 
